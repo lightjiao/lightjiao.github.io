1d7fd65f09bbad35d07a6219a6aa5a104228b71b
lightjiao <lightjiao@gmail.com>
2022-05-15 09:48:43 +0800

    发布博客

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -3,4 +3,4 @@ mkdir __Build/Blogs/
 
 cp ./Blogs/*.md ./__Build/Blogs/
 cp ./Scripts/index.html ./__Build/
-python ./Scripts/GenerateReadme.py > ./__Build/README.md
+python ./Scripts/GenerateReadme.py ./Blogs > ./__Build/README.md
